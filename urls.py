e6816d07f1be4cb712982cb2691042c627656420
wanglia101511 <wanglia@micron.com>
2024-05-27 17:37:34 +0800

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import url
+from django.urls import re_path as url
 from homepage import views
 
 
 
 urlpatterns=[
     url(r'^$', views.index, name='home'),
-    # url(r'^About$', views.about, name='about'),
-
 ]
